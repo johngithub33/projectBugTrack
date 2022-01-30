4f743c5131e7db95d32502138990afe6c1c1a6dd
john <johngithub33@gmail.com>
2022-01-30 12:46:58 -0500

    asdf

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 
+var mysql = require('mysql');
+
 //to parse incoming html form requests, the "body" of the request
 //this places the parsed body on the req.body object
 var bodyParser = require('body-parser')
@@ -9,8 +11,34 @@ app.use(bodyParser.urlencoded({ extended: false }))
 //serve static files, automatically serve index.html
 app.use(express.static(__dirname))
 
+// mysql
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: 'bugTracker',
+  });
+  
+  con.connect(function(err) { if (err) throw err;} )
+
 
 app.post('/signup', function(req, res){
+
+    let username = req.body.username;
+    let password = req.body.password;
+
+    // con.query("SELECT * FROM users WHERE EXISTS(SELECT * FROM USERS WHERE username = 'james')", function (err,result){
+
+        con.query(`SELECT * FROM users WHERE EXISTS(SELECT * FROM USERS WHERE username = '${username}')`, function (err,result){
+
+            if(err) throw err;
+
+            if(username == result[0].username && password == result[0].password){
+                console.log('logged in!')
+            }
+
+            else (console.log(result))
+    });
 
     //need to add root directory of __dirname to serve file on route here
     res.sendFile(__dirname + '/admin.html')
